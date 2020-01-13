fac08c764b1644d0af6574fd6add4839241c303d
Lizzie <lchan217@gmail.com>
2020-01-13 13:38:04 -0700

    move fetchrequest to app.js

--- src/components/WeatherContainer.js
+++ src/components/WeatherContainer.js
@@ -1,27 +1,6 @@
 import React, { Component } from "react";
 
 class WeatherContainer extends Component {
-  constructor() {
-    super();
-    this.state = {
-      list: [],
-      city: ""
-    };
-  }
-
-  componentDidMount() {
-    fetch(
-      "http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40"
-    )
-      .then(response => response.json())
-      .then(response =>
-        this.setState({
-          list: response.list,
-          city: response.city
-        })
-      );
-  }
-
   render() {
     return (
       <div>
