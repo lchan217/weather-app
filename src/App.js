e72d6e8f944b6b768b4ee2963b1d7e079831426d
Lizzie <lchan217@gmail.com>
2020-01-13 12:02:22 -0700

    add weather container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import WeatherContainer from "../src/components/WeatherContainer";
 
 function App() {
   return (
     <div className='App'>
-      <h1>App.js </h1>
+      <h1>Weather for Munich </h1>
+      <WeatherContainer />
     </div>
   );
 }
---
+++ src/components/WeatherContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class WeatherContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Weather Container</h1>
+      </div>
+    );
+  }
+}
+
+export default WeatherContainer;
