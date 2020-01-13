8125ba7e0b53c862c0ec8d66bdaf00c9ab3bd7c8
Lizzie <lchan217@gmail.com>
2020-01-13 16:38:16 -0700

    add skeleton of reducer

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+export default function rootReducer(state = [], action) {
+  switch (action.type) {
+    case "FETCH_WEATHER":
+      // will change later
+      return state;
+
+    default:
+      return state;
+  }
+}
