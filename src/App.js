f67599601126418728f1d36b6291b0fcd17219b2
Lizzie <lchan217@gmail.com>
2020-01-13 13:37:51 -0700

    move fetch req to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import WeatherContainer from "../src/components/WeatherContainer";
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Weather for Munich </h1>
-      <WeatherContainer />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      list: [],
+      city: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40"
+    )
+      .then(response => response.json())
+      .then(response =>
+        this.setState({
+          list: response.list,
+          city: response.city
+        })
+      );
+  }
+  render() {
+    return (
+      <div className='App'>
+        <h1>Weather for Munich </h1>
+        <WeatherContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
