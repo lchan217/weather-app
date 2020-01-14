b1cb8784cb7f20f1db779dc021a78167f486af94
Lizzie <lchan217@gmail.com>
2020-01-14 16:24:47 -0700

    map over obj to show date and temp

--- src/components/weather/WeatherCard.js
+++ src/components/weather/WeatherCard.js
@@ -4,6 +4,13 @@ const WeatherCard = ({ temps }) => {
   return (
     <div>
       <h1>WeatherCard</h1>
+      {Object.entries(temps).map(([date, temp]) => {
+        return (
+          <div>
+            date: {date} - temp: {temp.toFixed(2)}
+          </div>
+        );
+      })}
     </div>
   );
 };
