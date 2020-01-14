a1100e690c1bbde50c086ffc20e2d5c17a7aa433
Lizzie <lchan217@gmail.com>
2020-01-13 20:26:04 -0700

    add logic and assumptions for calcing avg temp

--- src/components/WeatherContainer.js
+++ src/components/WeatherContainer.js
@@ -3,15 +3,37 @@ import { connect } from "react-redux";
 import Radio from "@material-ui/core/Radio";
 
 class WeatherContainer extends Component {
+  show = () => {
+    //calculate average temp of dates available in api
+    //time that has already passed is not included in average
+    let tempObject = {}; //total temp by date
+    let countObject = {}; // total count by date
+    for (let item of this.props.list) {
+      let date = item.dt_txt.slice(0, 10);
+      tempObject[date]
+        ? (tempObject[date] += item.main.temp)
+        : (tempObject[date] = item.main.temp);
+
+      countObject[date] ? (countObject[date] += 1) : (countObject[date] = 1);
+    }
+
+    let averageObject = {};
+    for (let date in tempObject) {
+      averageObject[date] = tempObject[date] / countObject[date];
+    }
+  };
+
   render() {
     return (
       <div>
         <h1>Weather Container</h1>
         <ul>
           <li>c or f toggle</li>
-          {<Radio />} Celcius {<Radio />} Fahrenheit
-          <li>arrows</li>
-          <li>cards</li>
+          {<Radio />} Celcius {<Radio />} Fahrenheit <br />
+          <li>arrows</li> <br />
+          <li>Card Data</li>
+          {this.show()}
+          <br />
           <li>bar graph</li>
         </ul>
       </div>
