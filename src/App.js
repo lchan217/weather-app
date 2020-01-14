6e44bb9aa51920381d1c0bd5c2a455ab7d89d6ed
Lizzie <lchan217@gmail.com>
2020-01-13 17:33:52 -0700

    remove fetch req

--- src/App.js
+++ src/App.js
@@ -11,31 +11,20 @@ class App extends Component {
     super();
     this.state = {
       list: [],
-      city: "",
       isLoading: true
     };
   }
 
   componentDidMount() {
     this.props.fetchWeather();
-    // fetch(
-    //   "http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40"
-    // )
-    //   .then(response => response.json())
-    //   .then(response =>
-    //     this.setState({
-    //       list: response.list,
-    //       city: response.city,
-    //       isLoading: false
-    //     })
-    //   );
   }
+
   render() {
     let data;
     if (this.state.isLoading) {
       data = <Loading />;
     } else {
-      data = <WeatherContainer list={this.state.list} />;
+      data = <WeatherContainer />;
     }
     return <div className='App'>{data}</div>;
   }
