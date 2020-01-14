8e1e323cb230a5f2c8a61a1633b98267126393d3
Lizzie <lchan217@gmail.com>
2020-01-14 16:19:33 -0700

    add logic for showing 3 per page

--- src/components/weather/WeatherPagination.js
+++ src/components/weather/WeatherPagination.js
@@ -1,16 +1,32 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Icon } from "semantic-ui-react";
+import WeatherCard from "./WeatherCard";
 
-class WeatherPagination extends Component {
-  render() {
-    return (
-      <div>
-        {" "}
-        <Icon name='huge arrow circle left'></Icon>
-        <Icon name='huge arrow circle right'></Icon>
-      </div>
-    );
+const WeatherPagination = props => {
+  //start default page at 1
+  const [currentPage, setCurrentPage] = useState(1);
+
+  //3 temps per page
+  const [tempsPerPage, setTempsPerPage] = useState(3);
+
+  const indexOfLastTemp = currentPage * tempsPerPage;
+  const indexOfFirstTemp = indexOfLastTemp - tempsPerPage;
+
+  const keys = Object.keys(props.averages);
+  const tempsToShow = keys.slice(indexOfFirstTemp, indexOfLastTemp);
+  const currentTemps = {};
+
+  for (let date of tempsToShow) {
+    currentTemps[date] = props.averages[date];
   }
-}
+
+  return (
+    <div>
+      <Icon name='arrow circle left'></Icon>
+      <Icon name='arrow circle right'></Icon>
+      <WeatherCard temps={currentTemps} />
+    </div>
+  );
+};
 
 export default WeatherPagination;
