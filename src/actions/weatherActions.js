e18cb0ecf2b5e082705bf574ec3dd6546ce3b18a
Lizzie <lchan217@gmail.com>
2020-01-13 17:34:04 -0700

    add fetch req to action

--- src/actions/weatherActions.js
+++ src/actions/weatherActions.js
@@ -1,13 +1,9 @@
 export const fetchWeather = () => {
   return dispatch => {
-    debugger;
-    //   return fetch(
-    //     "http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40"
-    //   )
-    //     .then(response => response.json())
-    //     .then(response => response.json())
-    //     .then(payload => dispatch({ type: "FETCH_WEATHER", payload }));
+    return fetch(
+      "http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40"
+    )
+      .then(response => response.json())
+      .then(payload => dispatch({ type: "FETCH_WEATHER", payload }));
   };
 };
-
-// modify later
