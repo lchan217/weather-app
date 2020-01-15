4ec76579dfadb31fe946e5a9b528651bf3e9c048
Lizzie <lchan217@gmail.com>
2020-01-15 11:07:21 -0700

    add const data for barchart

--- src/components/weather/BarChart.js
+++ src/components/weather/BarChart.js
@@ -3,6 +3,31 @@ import { connect } from "react-redux";
 
 class BarChart extends Component {
   render() {
+    const date = this.props.date;
+    const allData = this.props.list;
+    const data = [];
+
+    for (let item of allData) {
+      if (date === item.dt_txt.slice(0, 10)) {
+        if (this.props.showF === true) {
+          data.push({
+            y: ((item.main.temp - 273.15) * 9) / 5 + 32,
+            x: item.dt_txt.slice(11, 19)
+          });
+        } else {
+          data.push({
+            y: item.main.temp - 273.15,
+            x: item.dt_txt.slice(11, 19)
+          });
+        }
+      }
+    }
+
+    const chartWidth = 800;
+    const chartHeight = 500;
+    //indicates lowest and highest point on y-axis
+    const chartDomain = [0, chartHeight];
+
     return <div>barchart</div>;
   }
 }
