061715578927c609a2adc6ce996d14da4598c4a7
Lizzie <lchan217@gmail.com>
2020-01-15 10:00:45 -0700

    add handleClick and handleClose functions

--- src/components/weather/WeatherCard.js
+++ src/components/weather/WeatherCard.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import { Card } from "semantic-ui-react";
+import BarChart from "./BarChart";
 
-const WeatherCard = ({ temps }) => {
-  return (
-    <div>
-      <Card.Group>
-        {Object.entries(temps).map(([date, temp]) => {
-          return (
-            <Card>
-              <Card.Content>
-                date: {date} - temp: {temp.toFixed(2)}
-              </Card.Content>
-            </Card>
-          );
-        })}
-      </Card.Group>
-    </div>
-  );
-};
+class WeatherCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showBar: false,
+      date: null
+    };
+  }
+  handleClick = date => {
+    this.setState({
+      showBar: true,
+      date: date
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      showBar: false
+    });
+  };
+
+  render() {
+    const { temps } = this.props;
+    const { handleClick, showBarChart } = this;
+    return (
+      <div>
+        <Card.Group>
+          {Object.entries(temps).map(([date, temp]) => {
+            return (
+              <Card onClick={() => handleClick(date)}>
+                <Card.Content>
+                  date: {date} - temp: {temp.toFixed(2)}
+                </Card.Content>
+              </Card>
+            );
+          })}
+        </Card.Group>
+      </div>
+    );
+  }
+}
 
 export default WeatherCard;
