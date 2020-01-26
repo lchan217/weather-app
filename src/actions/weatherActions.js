848c02b6277e64c0ce32f2a7ccc2dc554a9115ea
Lizzie <lchan217@gmail.com>
2020-01-26 10:31:47 -0700

    make fetch request secure by adding s

--- src/actions/weatherActions.js
+++ src/actions/weatherActions.js
@@ -1,7 +1,7 @@
 export const fetchWeather = () => {
   return dispatch => {
     return fetch(
-      "http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40"
+      "https://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40"
     )
       .then(response => response.json())
       .then(payload => dispatch({ type: "FETCH_WEATHER", payload }));
